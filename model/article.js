731a2b17a94c1f19f4e50cfea1f9931a357e753a
liuxianjie <2474827323@qq.com>
2020-02-12 01:04:28 +0800

    first version

---
+++ model/article.js
@@ -0,0 +1,16 @@
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+let articleSchema = new Schema({
+    title : {type:String,required:true}
+    ,tags : {type:String}
+    ,content : {type:String,required: true}
+    ,date : {type:Date,default:Date.now()}
+    ,author : {type:Schema.Types.ObjectId,ref:"user"}
+    ,readNum:{type:Number}
+    ,commentsNum:{type:Number}
+});
+
+module.exports = mongoose.model("article",articleSchema);
---
+++ routers/indexart.js
@@ -0,0 +1,23 @@
+
+const express = require("express");
+const article = require("../model/article");
+const comment = require("../model/comment");
+
+let router = express.Router();
+
+//访问文章页面
+module.exports = function(req,res){
+    article.find()
+    .then(data=>{
+        if ( data ){
+            res.render("index",{code:1,data});
+        } else{
+            res.render("index",{code:0,msg:"没有对应的文章",data:[]});
+        }
+    })
+    .catch(e=>{
+        console.log(e,333)
+        res.render("index",{code:0,msg:"服务器异常"});
+    });
+
+};
